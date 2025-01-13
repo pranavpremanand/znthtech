47350a86e71bc86441e9ace4f860f47825c537a6
Pranav <mpranavprem@gmail.com>
2025-01-13 18:05:58 +0530

    changes

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -27,7 +27,7 @@ const Services = ({ title, heading, data }) => {
             <div className="relative rotate-45 group-hover:rotate-0 transition-all duration-200 w-[4rem] h-[4rem] flex justify-center items-center bg-primary">
               <item.icon className="w-[2.5rem] -rotate-45 group-hover:rotate-0 transition-all duration-200 fill-white" />
             </div>
-            <p className="mt-3 font-bold">{item.title}</p>
+            <p className="mt-3 text-lg font-bold">{item.title}</p>
             <p>{item.desc}</p>
             {item.detailsPage && (
               <Link
